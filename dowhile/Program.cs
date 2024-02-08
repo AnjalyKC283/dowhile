9ef0757b2226d6f453e39db19dd834d66eb205c0
HP <HP@DESKTOP-OMHE841>
2024-02-08 10:40:50 +0530

    Add project files.

---
+++ dowhile/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dowhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, n;
+            Console.WriteLine("enter limit:");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("even numbers are:");
+            do
+            {
+                a = a + 2;
+                Console.WriteLine(a);
+            }
+            while (a <= n);
+            Console.ReadKey();
+        }
+    }
+}
